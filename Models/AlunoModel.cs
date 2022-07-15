d0ca64c48da7f193ca7865312c075644972848fc
Renata Kim <renata_kim@hotmail.com>
2022-07-15 15:38:40 -0300

    exercicio 30

---
+++ Models/AlunoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula30.Models
+{
+    public class AlunoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
---
+++ Models/EscolaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aula30.Models
+{
+    public class EscolaContext : DbContext
+    {
+        public EscolaContext(DbContextOptions<EscolaContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AlunoModel> Alunos { get; set; }
+        public DbSet<InstituicaoModel> Instituicoes { get; set; }
+    }
+
+}
